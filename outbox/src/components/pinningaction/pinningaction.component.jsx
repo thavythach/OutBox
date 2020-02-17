24c9dd0fc68482f560781554c6f780e10bc54c54
Thavy Thach <thavythach@gmail.com>
2020-02-17 15:28:28 -0600

    pin state completion

--- outbox/src/components/pinningaction/pinningaction.component.jsx
+++ outbox/src/components/pinningaction/pinningaction.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import Icon from '../../icons/pin/pin.svg.component';
+import IconButton from '@material-ui/core/IconButton';
+
 
 
 function PinningAction() {
+    const [state, setState] = React.useState({
+        pin: false,
+    });
+
+    const handleAction = () => {
+        setState({ ...state, pin: !state.pin});
+    };
+
     return(
         <div className="PinningAction">
             <p>hello</p>
-            <Icon icon="pin" color="blue"></Icon>
-            <Icon icon="pin" color="gray"></Icon>
+            <IconButton onClick={() => { handleAction() }}>
+                <Icon icon="pin" color={ state.pin ? "blue" : "gray"}></Icon>
+            </IconButton>
         </div>
     );
 }
