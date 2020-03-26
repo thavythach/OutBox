efb7bb55a46ab5b379dc2e37f7b2a097d8be6d17
Thavy Thach <thavythach@gmail.com>
2020-03-25 23:19:55 -0500

    (#17) email-list-items partially completed - we now need some real data

--- outbox/src/components/email-list/email-list.component.jsx
+++ outbox/src/components/email-list/email-list.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { List, ListItem, ListItemAvatar,ListItemSecondaryAction, ListItemText, Avatar, IconButton, Grid, EmailIcon, Container, ExpansionPanel, Typography, TextField, ExpansionPanelDetails,ExpansionPanelSummary, CssBaseline} from '@material-ui/core';
 
 import Actions from '../actions/actions.component';
-import { makeStyles } from '@material-ui/core/styles';
+import EmailListItem from './email-list-item/email-list-item.component';
 
 import './email-list.styles.css';
 
@@ -16,12 +16,17 @@ class EmailList extends React.Component {
     render(){
         
         return(
-            <div class="email_list">
+            <div class="email-list">
                 <React.Fragment>
                     <CssBaseline />
                     <Container maxWidth="xl">
                         <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
-                            <email-list-item></email-list-item>
+                            <EmailListItem class="email-list-item"></EmailListItem>
+                            <EmailListItem class="email-list-item"></EmailListItem>
+                            <EmailListItem class="email-list-item"></EmailListItem>
+                            <EmailListItem class="email-list-item"></EmailListItem>
+                            <EmailListItem class="email-list-item"></EmailListItem>
+                            <EmailListItem class="email-list-item"></EmailListItem>
                         </Typography>
                     </Container>
                 </React.Fragment>
