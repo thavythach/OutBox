30efa3930d797c73ceec769b5031434c902c7119
Thavy Thach <thavythach@gmail.com>
2020-08-30 11:34:52 -0500

    routinggit add ./git add ./

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,38 +1,18 @@
 import React from 'react';
 
-const Home: React.FC = () => {
-  return (
-    <h1>Home</h1>
-  );
-};
+import Home from './Home/Home';
 
-const Standings: React.FC = () => {
-  return (
-    <h1>Standings</h1>
-  );
-};
+export interface IRoute {
+  path: string;
+  sidebarName: string;
+  component: React.ComponentType;
+}
 
-const Teams: React.FC = () => {
-  return (
-    <h1>Teams</h1>
-  );
-};
-
-const Routes = [
+const Routes: IRoute[] = [
   {
     path: '/',
     sidebarName: 'Home',
     component: Home
-  },
-  {
-    path: '/standings',
-    sidebarName: 'Standings',
-    component: Standings
-  },
-  {
-    path: '/teams',
-    sidebarName: 'Teams',
-    component: Teams
   },
 ];
 
