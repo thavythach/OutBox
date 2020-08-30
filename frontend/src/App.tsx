eb6528617b9f00f4dfe757120bb9581f196420db
Thavy Thach <thavythach@gmail.com>
2020-08-30 11:29:57 -0500

    browser router added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,64 +1,21 @@
 import React from 'react';
-import './App.css';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
- } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import Drawer from '@material-ui/core/Drawer';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }),
-);
+import { Switch, Route } from 'react-router-dom';
+import Routes from './Routes';
+
+import NavigationBar from './NavigationBar/NavigationBar';
 
 const App: React.FC = () => {
-  const classes = useStyles();
-
-  const [isOpen, setIsOpen] = React.useState(false);
-  const toggleDrawer = (open: boolean) => (
-    event: React.KeyboardEvent | React.MouseEvent,
-  ) => {
-    if (
-      event.type === 'keydown' &&
-      ((event as React.KeyboardEvent).key === 'Tab' ||
-        (event as React.KeyboardEvent).key === 'Shift')
-      ) {
-        return;
-      }
-
-      setIsOpen(open);
-  };
-
   return (
     <div>
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              OutBox
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
-      <Drawer open={isOpen} onClose={toggleDrawer(false)}>
-        Hello Drawer!
-      </Drawer>
+      <NavigationBar />
+      <Switch>
+        {Routes.map((route: any) => (
+          <Route exact path={route.path} key={route.path}>
+            <route.component />
+          </Route>
+        ))}
+      </Switch>
     </div>
   );
 }
